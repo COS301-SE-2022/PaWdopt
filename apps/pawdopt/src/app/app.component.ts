7368b7f0706c7db82758fdb27b27009e6eb5ad6c
Kearon Foba <23222438+KearonF@users.noreply.github.com>
2022-05-02 20:27:42 +0200

    🚧 (UI): simple Ionic initialisation

---
+++ apps/pawdopt/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pawdopt-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  constructor() {}
+}
