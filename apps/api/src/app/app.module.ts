9dba09aa92a50a54c52702400360d3849c6abc37
Rayden Logan Burger <rayden.logan.viljoen@gmail.com>
2022-05-24 11:30:02 +0200

    Added API dependencies

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CoreModule } from "@pawdopt/core";
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
