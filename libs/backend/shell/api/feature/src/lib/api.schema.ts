1ba3bde30b499b800e168859339f16c068b086b4
Rayden Logan Burger <rayden.logan.viljoen@gmail.com>
2022-05-30 15:06:12 +0200

    Changed API Schema

--- libs/backend/shell/api/feature/src/lib/api.schema.ts
+++ libs/backend/shell/api/feature/src/lib/api.schema.ts
@@ -1,21 +1,17 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-class Characteristics {
-    lifespan: string
-    size: 'small' | 'medium' | 'large'
-    coat: 'short' | 'medium' | 'long'
-    color: string
-}
-
 @Schema()
-export class Cat {
+export class Dog {
+    @Prop()
+    name: string
+
     @Prop()
     breed: string;
     
     @Prop()
-    characteristics: Characteristics;
+    dob: Date;
 }
 
-export type CatDocument = Cat & Document;
-export const CatSchema = SchemaFactory.createForClass(Cat);
+export type DogDocument = Dog & Document;
+export const DogSchema = SchemaFactory.createForClass(Dog);
