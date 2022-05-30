0113b8867d28ce662cb9cd94d52853015f483694
Rayden Logan Burger <rayden.logan.viljoen@gmail.com>
2022-05-30 15:06:27 +0200

    Changed API Schema

--- libs/backend/shell/api/feature/src/lib/backend-shell-api-feature.module.ts
+++ libs/backend/shell/api/feature/src/lib/backend-shell-api-feature.module.ts
@@ -3,12 +3,12 @@ import { ApiService } from './api.service';
 import { ApiResolver } from './api.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { Cat, CatSchema } from './api.schema';
+import { Dog, DogSchema } from './api.schema';
 
 
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
+  imports: [MongooseModule.forFeature([{ name: Dog.name, schema: DogSchema }])],
   controllers: [],
   providers: [ApiService, ApiResolver],
   exports: [],
