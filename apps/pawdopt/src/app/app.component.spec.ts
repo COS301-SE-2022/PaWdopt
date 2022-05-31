7608c46de1d55f1efcda65ff39df575335cbf246
Kearon Foba <23222438+KearonF@users.noreply.github.com>
2022-05-31 14:43:57 +0200

    ✅  (CICD): Testing CICD

---
+++ apps/pawdopt/src/app/app.component.spec.ts
@@ -0,0 +1,9 @@
+describe('AppComponent', () => {
+    it('should work', () => {
+      expect(true).toBe(true);
+    });
+
+    it('should change stuff for codecov', () => {
+      expect(true).toBe(true);
+    });
+});
