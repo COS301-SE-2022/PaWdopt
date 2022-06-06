7df06d9b80fba7b134bdda7dc4397bbd062276ef
AlexK2010 <kusel.alex@gmail.com>
2022-06-06 14:25:01 +0200

    Added resolvers for queries

--- libs/backend/shell/api/feature/src/lib/api.resolver.ts
+++ libs/backend/shell/api/feature/src/lib/api.resolver.ts
@@ -1,31 +1,47 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { ApiService } from './api.service';
-import { DogType, OrganisationType, LocationType, UserType ,ImageType, ContactInfoType, DocType, AdopterType } from './api.dto';
-import { Dog, Image, Organisation, Location, User, ContactInfo, Doc, Adopter} from './api.schema';
+import { DogType, OrganisationType, LocationType, UserType ,PicType, ContactInfoType, DocType, AdopterType } from './api.dto';
+import { Dog, Pic, Organisation, Location, User, ContactInfo, Doc, Adopter} from './api.schema';
+import { Type } from '@angular/core';
 
 
 @Resolver()
 export class ApiResolver {
     constructor(private readonly DogService: ApiService) {}
 
-    @Query(returns => [DogType])
-    async Dog() {
-        return this.DogService.findAllDogs();
-    }
+    // @Query(returns => [DogType])
+    // async Dog() {
+    //     return this.DogService.findAllDogs();
+    // }
+
+    // @Query(returns => [DogType])
+    // async User() {
+    //     return this.DogService.findAllUser();
+    // }
+
+    // @Query(returns => [AdopterType])
+    // async Adopter() {
+    //     return this.DogService.findAllAdopter();
+    // }
 
     @Query(returns => [OrganisationType])
     async Organisation() {
         return this.DogService.findAllOrgs();
     }
 
-    @Query(returns => [DogType])
-    async User() {
-        return this.DogService.findAllUser();
+    @Mutation(returns => OrganisationType)
+    async createOrg(@Args('org') org: OrganisationType) : Promise<OrganisationType> {
+        return this.DogService.createOrg(org);
     }
 
-    @Query(returns => [AdopterType])
-    async Adopter() {
-        return this.DogService.findAllAdopter();
+    @Mutation(returns => OrganisationType)
+    async updateOrg(@Args ('name') name: string, @Args('org') org: OrganisationType) : Promise<OrganisationType> {
+        return this.DogService.updateOrg(name, org);
+    }
+
+    @Mutation(returns => OrganisationType)
+    async deleteOrg(@Args('name') name: string) : Promise<OrganisationType> {
+        return this.DogService.deleteOrg(name);
     }
 
 }
