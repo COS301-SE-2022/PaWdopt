072012e49f1c389b45a59655bcaa34edfbc7e54d
Aramnand <u20443626@tuks.co.za>
2022-06-06 10:02:45 +0200

    Fixed routing issues within update a dog

--- libs/mobile/updateorremovedog/feature/src/lib/updateorremovedog.page.ts
+++ libs/mobile/updateorremovedog/feature/src/lib/updateorremovedog.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ActionSheetController } from '@ionic/angular';
 @Component({
   selector: 'pawdopt-updateorremovedog',
   templateUrl: 'updateorremovedog.page.html',
@@ -7,7 +8,7 @@ import { Router } from '@angular/router';
 })
 export class updateorremovedogPageComponent {
   
-  constructor(private router: Router, ){}
+  constructor(private router: Router, public actionSheetController: ActionSheetController ){}
 
   // login(){
   //   // TODO Complete login validation
@@ -29,35 +30,35 @@ export class updateorremovedogPageComponent {
   //   this.router.navigate(["/addorg"]);
   // }
 
-  // async uploadPic(){
-  //   const actionSheet = await this.actionSheetController.create({
-  //     header: 'Upload picture',
-  //     buttons: [{
-  //       text: 'Take picture using your camera',
-  //       icon: 'camera-outline',
-  //       handler: () => {
-  //         console.log('Take picture clicked');
-  //       }
-  //     }, {
-  //       text: 'Choose a picture from your gallery',
-  //       icon: 'image-outline',
-  //       handler: () => {
-  //         console.log('Choose a picture clicked');
-  //       }
-  //     }, {
-  //       text: 'Cancel',
-  //       icon: 'close',
-  //       role: 'cancel',
-  //       handler: () => {
-  //         console.log('Cancel clicked');
-  //       }
-  //     }]
-  //   });
-  //   await actionSheet.present();
+  async uploadPic(){
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Upload picture',
+      buttons: [{
+        text: 'Take picture using your camera',
+        icon: 'camera-outline',
+        handler: () => {
+          console.log('Take picture clicked');
+        }
+      }, {
+        text: 'Choose a picture from your gallery',
+        icon: 'image-outline',
+        handler: () => {
+          console.log('Choose a picture clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
 
-  //   const { role, data } = await actionSheet.onDidDismiss();
-  //   console.log('onDidDismiss resolved with role and data', role, data);
-  // }
+    const { role, data } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role and data', role, data);
+  }
 
 }
 
