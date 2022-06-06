9b9cb5892cbf019aff1e6bf5db020be355534804
AlexK2010 <kusel.alex@gmail.com>
2022-06-06 14:26:35 +0200

    Commented out unit tests for now

---
+++ libs/backend/shell/api/feature/src/lib/api.service.spec.ts
@@ -0,0 +1,19 @@
+// import { expect } from 'chai';
+// import { ApiService } from './api.service';
+// import { Dog, Image, Organisation, Location, User, ContactInfo, Doc, Adopter } from './api.schema';
+// import { DogType, OrganisationType, LocationType, UserType ,ImageType, ContactInfoType, DocType, AdopterType } from './api.dto';
+
+
+// describe('ApiService', () => {
+//   let service: ApiService;
+
+//   beforeEach(() => {
+//     service = new ApiService();
+//   });
+
+//   it('should be defined', () => {
+//     expect(service).to.be.defined;
+//   });
+
+
+// });
