e3a0a0ff73d2c437a04c7342c7c74ab903fbb7ec
Rayden Logan Burger <rayden.logan.viljoen@gmail.com>
2022-06-07 18:23:49 +0200

    Co-authored-by: Chris Magerat <ChrisMagerat@users.noreply.github.com> Co-authored-by: Alex Küsel <u19236183@tuks.co.za> Co-authored-by: Aramnand <Aramnand@users.noreply.github.com> Co-authored-by: Kearon Foba <KearonF@users.noreply.github.com>

--- apps/pawdopt/src/app/home/home.module.ts
+++ apps/pawdopt/src/app/home/home.module.ts
@@ -7,6 +7,8 @@ import { HomePage } from './home.page';
 import { HomePageRoutingModule } from './home-routing.module';
 import { SwipeCardLibModule } from 'ng-swipe-card';
 
+
+
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule, SwipeCardLibModule],
   declarations: [HomePage],
