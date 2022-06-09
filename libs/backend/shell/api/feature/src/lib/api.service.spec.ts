20e04c34c8cc63d486985eeadbadd7337a84e065
AlexK2010 <kusel.alex@gmail.com>
2022-06-10 01:55:36 +0200

    Added unit testing for delete queries

--- libs/backend/shell/api/feature/src/lib/api.service.spec.ts
+++ libs/backend/shell/api/feature/src/lib/api.service.spec.ts
@@ -388,4 +388,148 @@ describe('ApiService', () => {
         });
     }
     );
+
+    //Test if deleteOrg(name: string) works and returns an Organisation
+    describe('deleteOrg', () => {
+        it('should return a Organisation', async () => {
+            jest
+                .spyOn(service, 'deleteOrg')
+                .mockImplementation((): Promise<Organisation> => Promise.resolve(org));
+
+            expect(await service.deleteOrg('name')).toMatchObject(org);
+        });
+
+        it('should return null', async () => {
+            jest.spyOn(service, 'deleteOrg').mockResolvedValue(null);
+
+            expect(await service.deleteOrg('name')).toEqual(null);
+        });
+    }
+    );
+
+    //Test if deleteDog(name: string) works and returns a Dog
+    describe('deleteDog', () => {
+        it('should return a Dog', async () => {
+            jest
+                .spyOn(service, 'deleteDog')
+                .mockImplementation((): Promise<Dog> => Promise.resolve(dog));
+
+            expect(await service.deleteDog('name')).toMatchObject(dog);
+        });
+
+        it('should return null', async () => {
+            jest.spyOn(service, 'deleteDog').mockResolvedValue(null);
+
+            expect(await service.deleteDog('name')).toEqual(null);
+        });
+    }
+    );
+
+    //Test if deleteAdopter(name: string) works and returns an Adopter
+    describe('deleteAdopter', () => {
+        it('should return an Adopter', async () => {
+            jest
+                .spyOn(service, 'deleteAdopter')
+                .mockImplementation((): Promise<Adopter> => Promise.resolve(adopter));
+
+            expect(await service.deleteAdopter('name')).toMatchObject(adopter);
+        });
+
+        it('should return null', async () => {
+            jest.spyOn(service, 'deleteAdopter').mockResolvedValue(null);
+
+            expect(await service.deleteAdopter('name')).toEqual(null);
+        });
+    }
+    );
+
+    //Test if deletePic(name: string) works and returns a Pic
+    describe('deletePic', () => {
+        it('should return a Pic', async () => {
+            jest
+                .spyOn(service, 'deletePic')
+                .mockImplementation((): Promise<Pic> => Promise.resolve(pic));
+
+            expect(await service.deletePic('name')).toMatchObject(pic);
+        });
+
+        it('should return null', async () => {
+            jest.spyOn(service, 'deletePic').mockResolvedValue(null);
+
+            expect(await service.deletePic('name')).toEqual(null);
+        });
+    }
+    );
+
+    //Test if deleteDoc(name: string) works and returns a Doc
+    describe('deleteDoc', () => {
+        it('should return a Doc', async () => {
+            jest
+                .spyOn(service, 'deleteDoc')
+                .mockImplementation((): Promise<Doc> => Promise.resolve(doc));
+
+            expect(await service.deleteDoc('name')).toMatchObject(doc);
+        });
+
+        it('should return null', async () => {
+            jest.spyOn(service, 'deleteDoc').mockResolvedValue(null);
+
+            expect(await service.deleteDoc('name')).toEqual(null);
+        });
+    }
+    );
+
+    //Test if deleteContactInfo(name: string) works and returns a ContactInfo
+    describe('deleteContactInfo', () => {
+        it('should return a ContactInfo', async () => {
+            jest
+                .spyOn(service, 'deleteContactInfo')
+                .mockImplementation((): Promise<ContactInfo> => Promise.resolve(contInfo));
+
+            expect(await service.deleteContactInfo('name')).toMatchObject(contInfo);
+        });
+
+        it('should return null', async () => {
+            jest.spyOn(service, 'deleteContactInfo').mockResolvedValue(null);
+
+            expect(await service.deleteContactInfo('name')).toEqual(null);
+        });
+    }
+    );
+
+    //Test if deleteLocation(name: string) works and returns a Location
+    describe('deleteLocation', () => {
+        it('should return a Location', async () => {
+            jest
+                .spyOn(service, 'deleteLocation')
+                .mockImplementation((): Promise<Location> => Promise.resolve(loc));
+
+            expect(await service.deleteLocation('name')).toMatchObject(loc);
+        });
+
+        it('should return null', async () => {
+            jest.spyOn(service, 'deleteLocation').mockResolvedValue(null);
+
+            expect(await service.deleteLocation('name')).toEqual(null);
+        });
+    }
+    );
+
+    //Test if deleteOrgMember(name: string) works and returns an OrgMember
+    describe('deleteOrgMember', () => {
+        it('should return an OrgMember', async () => {
+            jest
+                .spyOn(service, 'deleteOrgMember')
+                .mockImplementation((): Promise<OrgMember> => Promise.resolve(orgMem));
+
+            expect(await service.deleteOrgMember('name')).toMatchObject(orgMem);
+        });
+
+        it('should return null', async () => {
+            jest.spyOn(service, 'deleteOrgMember').mockResolvedValue(null);
+
+            expect(await service.deleteOrgMember('name')).toEqual(null);
+        });
+    }
+    );
 });
