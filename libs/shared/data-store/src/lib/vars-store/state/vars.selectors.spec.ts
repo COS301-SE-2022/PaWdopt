98af6d1c3a4cb7372c92b27230ac9680772ffa37
AlexK2010 <kusel.alex@gmail.com>
2022-07-20 15:36:48 +0200

    Revert "Commented out testing for state management library"
    This reverts commit 4033bc1df99b91fed3c717fe9f730086ee7759b6.

--- libs/shared/data-store/src/lib/vars-store/state/vars.selectors.spec.ts
+++ libs/shared/data-store/src/lib/vars-store/state/vars.selectors.spec.ts
@@ -1,62 +1,62 @@
-// import { VarsEntity } from './vars.models';
-// import { varsAdapter, VarsPartialState, initialState } from './vars.reducer';
-// import * as VarsSelectors from './vars.selectors';
+import { VarsEntity } from './vars.models';
+import { varsAdapter, VarsPartialState, initialState } from './vars.reducer';
+import * as VarsSelectors from './vars.selectors';
 
-// describe('Vars Selectors', () => {
-//   const ERROR_MSG = 'No Error Available';
-//   const getVarsId = (it: VarsEntity) => it.id;
-//   const createVarsEntity = (id: string, name = '') =>
-//     ({
-//       id,
-//       name: name || `name-${id}`,
-//     } as VarsEntity);
+describe('Vars Selectors', () => {
+  const ERROR_MSG = 'No Error Available';
+  const getVarsId = (it: VarsEntity) => it.id;
+  const createVarsEntity = (id: string, name = '') =>
+    ({
+      id,
+      name: name || `name-${id}`,
+    } as VarsEntity);
 
-//   let state: VarsPartialState;
+  let state: VarsPartialState;
 
-//   beforeEach(() => {
-//     state = {
-//       vars: varsAdapter.setAll(
-//         [
-//           createVarsEntity('PRODUCT-AAA'),
-//           createVarsEntity('PRODUCT-BBB'),
-//           createVarsEntity('PRODUCT-CCC'),
-//         ],
-//         {
-//           ...initialState,
-//           selectedId: 'PRODUCT-BBB',
-//           error: ERROR_MSG,
-//           loaded: true,
-//         }
-//       ),
-//     };
-//   });
+  beforeEach(() => {
+    state = {
+      vars: varsAdapter.setAll(
+        [
+          createVarsEntity('PRODUCT-AAA'),
+          createVarsEntity('PRODUCT-BBB'),
+          createVarsEntity('PRODUCT-CCC'),
+        ],
+        {
+          ...initialState,
+          selectedId: 'PRODUCT-BBB',
+          error: ERROR_MSG,
+          loaded: true,
+        }
+      ),
+    };
+  });
 
-//   describe('Vars Selectors', () => {
-//     it('getAllVars() should return the list of Vars', () => {
-//       const results = VarsSelectors.getAllVars(state);
-//       const selId = getVarsId(results[1]);
+  describe('Vars Selectors', () => {
+    it('getAllVars() should return the list of Vars', () => {
+      const results = VarsSelectors.getAllVars(state);
+      const selId = getVarsId(results[1]);
 
-//       expect(results.length).toBe(3);
-//       expect(selId).toBe('PRODUCT-BBB');
-//     });
+      expect(results.length).toBe(3);
+      expect(selId).toBe('PRODUCT-BBB');
+    });
 
-//     it('getSelected() should return the selected Entity', () => {
-//       const result = VarsSelectors.getSelected(state) as VarsEntity;
-//       const selId = getVarsId(result);
+    it('getSelected() should return the selected Entity', () => {
+      const result = VarsSelectors.getSelected(state) as VarsEntity;
+      const selId = getVarsId(result);
 
-//       expect(selId).toBe('PRODUCT-BBB');
-//     });
+      expect(selId).toBe('PRODUCT-BBB');
+    });
 
-//     it('getVarsLoaded() should return the current "loaded" status', () => {
-//       const result = VarsSelectors.getVarsLoaded(state);
+    it('getVarsLoaded() should return the current "loaded" status', () => {
+      const result = VarsSelectors.getVarsLoaded(state);
 
-//       expect(result).toBe(true);
-//     });
+      expect(result).toBe(true);
+    });
 
-//     it('getVarsError() should return the current "error" state', () => {
-//       const result = VarsSelectors.getVarsError(state);
+    it('getVarsError() should return the current "error" state', () => {
+      const result = VarsSelectors.getVarsError(state);
 
-//       expect(result).toBe(ERROR_MSG);
-//     });
-//   });
-// });
+      expect(result).toBe(ERROR_MSG);
+    });
+  });
+});
