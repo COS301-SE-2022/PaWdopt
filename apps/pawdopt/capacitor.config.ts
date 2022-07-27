f18b848828720f6c4504fbb2727ed81b9fcf22c3
Kearon Foba <u13312694@tuks.co.za>
2022-07-27 16:10:24 +0200

    🚧 (Capacitor): Enabled cleartext temorarily for native feature development before deployment

--- apps/pawdopt/capacitor.config.ts
+++ apps/pawdopt/capacitor.config.ts
@@ -1,10 +1,15 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.cloud5.pawdopt',
   appName: 'pawdopt',
   webDir: '../../dist/apps/pawdopt',
   bundledWebRuntime: false,
+  server: {
+    cleartext: true,
+  }
 };
+
+//! REMEMBER TO REMOVE CLEARTEXT WHEN READY FOR PRODUCTION
 
 export default config;
