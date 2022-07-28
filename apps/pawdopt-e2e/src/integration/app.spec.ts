593bde905eb87ae2e0ed117ce11c67ab171beddc
Kearon Foba <u13312694@tuks.co.za>
2022-07-28 22:36:55 +0200

    🎨 (testing): Added ids to fields for testing

--- apps/pawdopt-e2e/src/integration/app.spec.ts
+++ apps/pawdopt-e2e/src/integration/app.spec.ts
@@ -1,27 +1,73 @@
 
 describe('Adopter Use Case', () => {
+  it.skip('should add adopter', () => {
+    cy.visit("/login");
+    cy.get("#signup-link").click();
+    cy.url().should('include', '/signup');
+    // Fill in form
+    cy.get('#username-field > .native-input').type('Testerman');
+    cy.get('#password-field > .native-input').type('123456');
+    cy.get('#re-enter-password-field > .native-input').type('123456');
+    cy.get('#idnum-field > .native-input').type('123456789012');
+    cy.get('#email-field > .native-input').type('testerman@ci.mintemail.com');
+    // Check
+    cy.get('#username-field > .native-input').should('have.value', 'Testerman');
+    cy.get('#password-field > .native-input').should('have.value', '123456');
+    cy.get('#re-enter-password-field > .native-input').should('have.value', '123456');
+    cy.get('#idnum-field > .native-input').should('have.value', '123456789012');
+    cy.get('#email-field > .native-input').should('have.value', 'testerman@ci.mintemail.com');
+    // Submit
+    // cy.get('#create-account-button').click();
+  });
 
   it('should login as adopter', () => {
     cy.visit('/login');
-    cy.get('#email-field > .native-input').type('Testerman');
+    cy.get('#email-field > .native-input').type('testerman@ci.mintemail.com');
     cy.get('#password-field > .native-input').type('123456');
-    cy.get('#email-field > .native-input').should('have.value', 'Testerman');
+    cy.get('#email-field > .native-input').should('have.value', 'testerman@ci.mintemail.com');
     cy.get('#password-field > .native-input').should('have.value', '123456');
     cy.get('#login-button').click();
     cy.url().should('include', '/home');
   });
 
-  it('should swipe on some cards', () => {
+  it.skip('should swipe on some cards', () => {
       cy.get("#like-button").click();
       cy.get(".card").last().should("not.be.visible")
       cy.get("#dislike-button").click();
       cy.get(".card").last().prev().should("not.be.visible");
   });
 
+  it.skip('should retry a dog', () => {
+      cy.get("#retry-button").click();
+      cy.get(".card").last().should("be.visible");
+      cy.get("#like-button").click();
+  });
+
+  it('should navigate to likeddogs', () => {
+      cy.get("#tab-button-liked").click();
+      cy.url().should("include", "/likeddogs");
+  });
+
+
+
+
   // it('should retry', (){
   //     cy.get("")
   // });
 
 
-});  
-//hello
+});
+
+describe.skip('OrgMember Use Case', () => {
+  it('should add org member', () => {
+    cy.visit("/login");
+    cy.get("#org-signup-link").click();
+    cy.url().should('include', '/addorg');
+    // Fill in form
+    cy.get('#username-field > .native-input').type('Testerman');
+    cy.get('#password-field > .native-input').type('123456');
+    cy.get('#re-enter-password-field > .native-input').type('123456');
+    cy.get('#idnum-field > .native-input').type('123456789012');
+    cy.get('#email-field > .native-input').type('');
+  });
+});
