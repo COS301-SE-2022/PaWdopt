140deaab1d93178fc202049fa66acbb09a9f58a2
AlexK2010 <u19236183@tuks.co.za>
2022-07-28 10:45:36 +0200

    🚀 Added preferences page

---
+++ libs/mobile/preferences/feature/src/lib/preferences.page.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActionSheetController } from '@ionic/angular';
+
+@Component({
+  selector: 'pawdopt-preferences',
+  templateUrl: 'preferences.page.html',
+  styleUrls: ['preferences.page.scss', '../../../../../shared/styles/global.scss'],
+  providers: [],
+})
+export class PreferencesPageComponent {
+  gender!: string;
+  breed!: string;
+  size!: {
+    lower: number;
+    upper: number;
+  };
+  sizeAny!: boolean;
+
+  constructor(private router: Router, public actionSheetController: ActionSheetController) {
+    this.gender = "any";
+    this.breed = "any";
+    this.size = {
+      lower: 0,
+      upper: 100
+    };
+    this.sizeAny = true;
+  }
+
+  Back(){
+    // TODO Complete add dog validation
+    this.router.navigate(["/owneddogs"]);
+  }
+
+  home(){
+    this.router.navigate(["/owneddogs"]);
+  }
+
+  likeddogs(){
+    this.router.navigate(["/adoptionprocess"]);
+  }
+
+  profile(){
+    this.router.navigate(["/orgprofile"]);
+  }
+
+  preferences(){
+    //this.router.navigate(["/orgsettings"]); Not implemented yet
+  }
+}
