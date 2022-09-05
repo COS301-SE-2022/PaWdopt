f490e1dbba2e6c60d53a6cad584b925f4bb1be36
Kearon Foba <u13312694@tuks.co.za>
2022-09-05 14:49:18 +0200

    🔐  (env): Updated to correct db url

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -5,8 +5,10 @@ import { GraphQLModule} from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
- 
 
+import * as dotenv from 'dotenv';
+dotenv.config();
+ 
 @Module({
   imports: [
     BackendShellApiFeatureModule,
@@ -14,7 +16,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql'
     }),
-    MongooseModule.forRoot(process.env.DB_URL+"nest"),
+    MongooseModule.forRoot(process.env.DB_URL),
   ],
   controllers: [AppController],
   providers: [AppService],
