ff86e497ea694b7226858ac722904cea0d508d4f
Kearon Foba <u13312694@tuks.co.za>
2022-09-27 21:40:05 +0200

    ⚰️ 🎨 (Tidy): Removed code stink

--- libs/backend/shell/api/feature/src/lib/backend-shell-api-feature.module.spec.ts
+++ libs/backend/shell/api/feature/src/lib/backend-shell-api-feature.module.spec.ts
@@ -10,8 +10,6 @@ dotenv.config();
 describe('BackendShellApiFeatureModule', () => {
 
     let app: INestApplication;
-    let apiService: ApiService;
-    let adopter: Adopter;
     
     beforeAll(async () => {
         const moduleRef = await Test.createTestingModule({
@@ -23,48 +21,13 @@ describe('BackendShellApiFeatureModule', () => {
         app = moduleRef.createNestApplication();
         await app.init();
 
-        apiService = app.get(ApiService);
+        app.get(ApiService);
     });
     
     it('should create an instance', () => {
         expect(app).toBeDefined();
     });
-
-    // it('should create an adopter', async () => {
-    //     adopter = {
-    //         _id: 'testID',
-    //         name: 'John Doe',
-    //         email: 'johntesterman@test.com',
-    //         IDNum: '1234567890123',
-    //         pic: '',
-    //         location: {
-    //             lat: 0,
-    //             lng: 0
-    //         },
-    //         documents: null,
-    //         dogsLiked: null,
-    //         dogsDisliked: null,
-    //         uploadedDocs: false
-    //     };
-    //     expect((await apiService.createAdopter(adopter)).name).toEqual(adopter.name);
-    // });
-
-    // it('should get an adopter', async () => {
-    //     const req = "johntesterman@test.com";
-    //     adopter = await apiService.findAdopter(req);
-    //     expect(adopter.email).toBe(req);
-    // });
-
-    // it('should update an adopter', async () => {
-    //     adopter.email = "johntesterman@updatedtest.com";
-    //     expect((await apiService.updateAdopter(adopter._id, adopter)).email).toBeTruthy();
-    // });
     
-    // it('should delete an adopter', async () => {
-    //     adopter = await apiService.findAdopter("johntesterman@updatedtest.com");
-    //     expect(await apiService.deleteAdopter(adopter._id)).toEqual(adopter);
-    // });
-
     afterAll(async () => {
         await app.close();
     });
